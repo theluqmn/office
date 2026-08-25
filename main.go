4395d287ec118263cf6e5930bfb91c7cd6665a06
theluqmn <theluqmn@outlook.com>
2026-08-25 08:23:55 +0800

    initialised go echo

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"fmt";
+
+	"io";
+	"html/template"
+	"github.com/labstack/echo/v4"
 )
+
+type Template struct { templates *template.Template }
+
+func (t *Template) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	fmt.Println("Gello world!")
+
+	renderer := &Template{ templates: template.Must(template.ParseGlob("views/*.html")) }
+
+	// echo instance config
+	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+	e.Renderer = renderer
+
+	// serving routes
+	e.Static("/static", "static")
+	
+	fmt.Println("Now online")
+	e.Logger.Fatal(e.Start(":8080"))
 }
